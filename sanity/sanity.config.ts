1b0b36faa10351d187acf18eea1d2467c27558e2
Mitchell Christ <xzmaru@gmail.com>
2024-01-03 18:54:39 -0800

    add dashboard

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { dashboardTool, projectInfoWidget, projectUsersWidget } from '@sanity/dashboard'
+import { vercelWidget } from 'sanity-plugin-dashboard-widget-vercel'
 
 export default defineConfig({
 	name: 'default',
@@ -10,7 +12,13 @@ export default defineConfig({
 	projectId: 'xnvjcxa4',
 	dataset: 'production',
 
-	plugins: [deskTool(), visionTool()],
+	plugins: [
+		deskTool(),
+		dashboardTool({
+			widgets: [projectInfoWidget(), projectUsersWidget(), vercelWidget()],
+		}),
+		visionTool(),
+	],
 
 	schema: {
 		types: schemaTypes,
