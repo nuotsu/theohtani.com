d548c314056b00f12865707881934b14957b4414
Mitchell Christ <xzmaru@gmail.com>
2024-01-04 19:09:29 -0800

    add header blur

--- kit/src/lib/Header.svelte
+++ kit/src/lib/Header.svelte
@@ -1,6 +1,15 @@
-<header class="p-4">
+<header
+	class="sticky top-0 z-10 p-4 bg-gradient-to-b from-white from-25% to-transparent before:absolute"
+>
 	<Logo class="mx-auto" />
 </header>
+
+<style lang="postcss">
+	header::before {
+		mask-image: linear-gradient(to bottom, #fff, #fff, transparent);
+		@apply pointer-events-none -bottom-12 inset-0 backdrop-blur;
+	}
+</style>
 
 <script>
 	import Logo from './Logo.svelte'
